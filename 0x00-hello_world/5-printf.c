66774af14615aa4c8b648680e3fe71342f7c0b28
Antony <antonybabaleohenry@gmail.com>
2022-12-11 07:02:58 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main -entry point
+ * Return 0 (success)
+ */
+
+int main (void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+
+
+}
