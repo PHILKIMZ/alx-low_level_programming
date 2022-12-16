2322ca17eb3d07a540dd18918b67728df299b3c8
Antony <antonybabaleohenry@gmail.com>
2022-12-15 18:57:57 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - short description, single line
+ * @c: contains values to be compaired
+ */
+
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
